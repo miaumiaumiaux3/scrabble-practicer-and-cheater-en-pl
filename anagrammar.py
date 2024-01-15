ccb53b49600cc285680062e44b08499d42022d97
Tifa <tifcha1@st.amu.edu.pl>
2024-01-15 16:43:37 +0100

    gameified print statements and inputs successfully

--- anagrammar.py
+++ anagrammar.py
@@ -2,82 +2,57 @@
 import random
 #import letter_scores
 
-#choose language, global var AF
-#language = 'en' #267,753 words
-language = 'pl' #works but WARNING because of cases and genders, there are over 3 MILLION entries 
+#langauges movedto classes
+class English(): #267,753 words
+    language = 'en'
+    filepath = 'sowpods.txt' 
+    bag_of_letter_tiles = list('?'*2 + 'e'*12 + 'a'*9 + 'i'*9 + 'o'*8 + 'n'*6 + 'r'*6 + 't'*6 + 'l'*4 
+                            + 's'*4 + 'u'*4 + 'd'*4 + 'g'*3 + 'b'*2 + 'c'*2 + 'm'*2 + 'p'*2 + 'f'*2 
+                            + 'h'*2 + 'v'*2 + 'w'*2 + 'y'*2 + 'kjxqz')
+    letter_points = {
+        'e':1, 'a':1, 'i':1, 'o':1, 'n':1, 'r':1, 't':1, 'l':1, 's':1, 'u':1,
+        'd':2, 'g':2, 
+        'b':3, 'c':3, 'm':3, 'p':3, 
+        'f':4, 'h':4, 'v':4, 'w':4, 'y':4, 
+        'k':5, 
+        'j':8, 'x':8,
+        'q':10, 'z':10,
+        '?':0
+    }
 
-class GameLanguage:
-    def __init__(self, language, filepath, letters, letterscores):
-        self.language = language
-        self.filepath = filepath
-        self.letters = letters
-        self.letterscores = letterscores
+class Polish(): #works but WARNING because of cases and genders, there are over 3 MILLION entries 
+    language = 'pl'
+    filepath = 'slowa.txt'
+    bag_of_letter_tiles = list('?'*2 + 'a'*9 + 'i'*8 +'e'*7 + 'o'*6 + 'n'*5 + 'z'*5 + 'r'*4 + 's'*4 + 'w'*4  +'y'*4 
+                    + 'c'*3 + 'd'*3 + 'k'*3 + 'l'*3 + 'm'*3 + 'p'*3 + 't'*3 + 'b'*2 + 'g'*2 + 'h'*2 
+                    + 'j'*2 + 'ł'*2 + 'u'*2 + 'ąęfóśżćńź')
+    letter_points = {
+        'a':1, 'i':1, 'e':1, 'o':1, 'n':1, 'z':1, 'r':1, 's':1, 'w':1,
+        'y':2, 'c':2, 'd':2, 'k':2, 'l':2, 'm':2, 'p':2, 't':2,
+        'b':3, 'g':3, 'h':3, 'j':3, 'ł':3, 'u':3,
+        'ą':5, 'ę':5, 'f':5, 'ó':5, 'ś':5, 'ż':5,
+        'ć':6,
+        'ń':7,
+        'ź':9,
+        '?':0
+    }
 
-
-#dict of all letter points (don't think there's a way to combine it and probs not worth)
-letter_points_en = {
-    'e':1, 'a':1, 'i':1, 'o':1, 'n':1, 'r':1, 't':1, 'l':1, 's':1, 'u':1,
-    'd':2, 'g':2, 
-    'b':3, 'c':3, 'm':3, 'p':3, 
-    'f':4, 'h':4, 'v':4, 'w':4, 'y':4, 
-    'k':5, 
-    'j':8, 'x':8,
-    'q':10, 'z':10,
-    '?':0
-}
-
-letter_points_pl = {
-    'a':1, 'i':1, 'e':1, 'o':1, 'n':1, 'z':1, 'r':1, 's':1, 'w':1,
-    'y':2, 'c':2, 'd':2, 'k':2, 'l':2, 'm':2, 'p':2, 't':2,
-    'b':3, 'g':3, 'h':3, 'j':3, 'ł':3, 'u':3,
-    'ą':5, 'ę':5, 'f':5, 'ó':5, 'ś':5, 'ż':5,
-    'ć':6,
-    'ń':7,
-    'ź':9,
-    '?':0
-}
 
 class Player:
     def __init__(self, name ='Player'):
         self.name = name
         self.hand = []
-        
-    #     self._hand = []
-
-    # @property
-    # def hand(self):
-    #     return self._hand
-
-    # @hand.setter
-    # def hand(self, newhand):
-    #     self._hand = newhand
 
 
 def create_lexicon():
-    if language == 'en':
-        #Read from sowpods.txt and populate list
-        with open ('sowpods.txt', 'r', encoding = 'utf-8') as file:
+    #Read from filepath and populate list
+    with open (language.filepath, 'r', encoding = 'utf-8') as file:
             return [line.rstrip() for line in file] #rstrip to remove whitespace line by line
-    elif language == 'pl':
-        with open ('slowa.txt', 'r', encoding = 'utf-8') as file:
-            return [line.rstrip() for line in file] 
-    else:
-        #invalid language, but should be impossible because global var
-        pass
 
 
 def fill_tilebag():
 #populate the tilebag list with the predetermined amount of each tile
-    if language == 'en':
-        return list('?'*2 + 'e'*12 + 'a'*9 + 'i'*9 + 'o'*8 + 'n'*6 + 'r'*6 + 't'*6 + 'l'*4 + 's'*4 + 'u'*4 
-                  + 'd'*4 + 'g'*3 + 'b'*2 + 'c'*2 + 'm'*2 + 'p'*2 + 'f'*2 + 'h'*2 + 'v'*2 + 'w'*2 + 'y'*2
-                  + 'kjxqz')
-    elif language == 'pl':
-        return list('?'*2 + 'a'*9 + 'i'*8 +'e'*7 + 'o'*6 + 'n'*5 + 'z'*5 + 'r'*4 + 's'*4 + 'w'*4  +'y'*4 
-                    + 'c'*3 + 'd'*3 + 'k'*3 + 'l'*3 + 'm'*3 + 'p'*3 + 't'*3 + 'b'*2 + 'g'*2 + 'h'*2 
-                    + 'j'*2 + 'ł'*2 + 'u'*2 + 'ąęfóśżćńź')
-    else:
-        print('Invalid language string, though that should be impossible since its a global variable')
+    return language.bag_of_letter_tiles
 
 
 def valid_word_checker(tiles, wilds, word):
@@ -90,10 +65,7 @@ def valid_word_checker(tiles, wilds, word):
     #print(f"tiles {temp_tiles}")
     for letter in word:
         if letter in temp_tiles:
-            if language == 'en':
-                wordscore += letter_points_en[letter]
-            elif language == 'pl':
-                wordscore += letter_points_pl[letter]
+            wordscore += language.letter_points[letter]
             temp_tiles.remove(letter)
             #print(f'tiles {temp_tiles} word {word} wordscore = {wordscore}')
         elif len(temp_tiles) == 0:
@@ -101,11 +73,11 @@ def valid_word_checker(tiles, wilds, word):
         else:
             miss_counter += 1 
             #word_score += 0
-            #invalid word, go to next word in list
+            
     if miss_counter <= wilds:
         return wordscore
     else:
-        return -1
+        return -1 #invalid word
 
 
 def validate_and_score_words(tiles, lexicon):
@@ -127,6 +99,16 @@ def validate_and_score_words(tiles, lexicon):
     return valid_words #dict of word:score
 
 
+#ok, let's get started!
+desired_language = input("English(en) or Polish(pl)?\n")
+if desired_language == 'en' or desired_language.lower() == 'english':
+    language = English()
+elif desired_language == 'pl' or desired_language.lower() == 'polish' or desired_language.lower() == 'polski':
+    language = Polish()
+else:
+    print('Invalid input')
+
+print('Scrabble Practice! Try to get the highest scoring word from your available tiles!') #until the tiles run out? Can do but later
 
 #read dictionary of valid words from file and put into list
 lexicon = create_lexicon()
@@ -143,30 +125,33 @@ player1 = Player() #if more than one player, on creation of second player, set p
 
 #give 7 tiles to player's hand and pop those tiles out of tilebag
 player1.hand = tilebag[0:7]
-print(player1.hand)
 tilebag = tilebag[7:]
+print(f'Your hand: {player1.hand}')
 #print(tilebag)
+
+typed_word = input("Type a word to score!\n")
 
 validated_words = validate_and_score_words(player1.hand, lexicon)
 
-print(dict(sorted(validated_words.items(), key=lambda item: item[1], reverse=True)))
+if len(validated_words) == 0:
+    print("Whoa, there are no valid words in this hand. That's not supposed to happen!")
+elif typed_word in validated_words: 
+    wordscore = validated_words[typed_word]
+    print(f"Nice! You scored {wordscore} points!")
+    
+    #create list of all values (possible scores) from validated words, and use set() to remove duplicates
+    possible_scores = list(set(validated_words.values())) 
+    possible_scores.sort(reverse=True)
+
+    #determine rank of typed_word, compared to other possible scores
+    rank = possible_scores.index(wordscore) + 1
+    if rank == possible_scores[0]:
+        print("Wow! You nailed it! You hit the top rank of points with the maximum possible score!")
+    else:
+        print(f'Your word score rank was {rank}! (The top score at rank 1 was: {possible_scores[0]})')
+else:
+    print('Invalid word...')
+    print('Here were all the possible words, sorted from highest to lowest score:')
+    print(dict(sorted(validated_words.items(), key=lambda item: item[1], reverse=True)))
 
 
-#current error
-#['e', 'e', 'c', 'b', 'a', 'a', 'd']
-#{'abecedarian': 12, 'abecedarians': 12, <- literally impossible, so why is it passing? do we need to do a length check?
-#for now I'm doing a length check, and if that stops working in a future iteration, I'll come up with another solution
-
-#current error
-#['r', 'm', 'i', 'i', 'i', 'a', 'ł']
-#{'armii': 6, 'ramii': 6, 'amii': 5, 'miar': 5, 'mira': 5, 'rami': 5, 'arii': 4, 'ima': 4, 'mai': 4, 'mar': 4, 'mir': 4, 'ram': 4, 'air': 3, 'am': 3, 'iii': 3, 'im': 3, 'ma': 3, 'mi': 3, 'rai': 3, 'ar': 2, 'ii': 2}
-#where is "miał" ?
-#ERROR INLOVES POLSKI ZNACZKI
-
-#['c', 'ł', 'm', 'a', 'm', 'ź', 'y']
-#{'cyma': 7, 'macy': 7, 'mamy': 7, 'myca': 7, 'cym': 6, 'myc': 6, 'mym': 6, 'mac': 5, 'mam': 5, 'may': 5, 'yam': 5, 'my': 4, 'am': 3, 'ma': 3} 
-#NIE MA ANI ł ANI Ź
-
-#FIX WAS SUPER EASY!! these kinds of character are EXACTLY why it's worth opening the file with encoding = 'utf-8' SOLVED YAYY
-#['ż', 'ó', 'w', 'ź', 'j', 'z', 'w'] 
-#{'wwóź': 16, 'zwóź': 16, 'wóź': 15, 'zwój': 10, 'wwóz': 8, 'zwów': 8, 'wóz': 7, 'ów': 6}
